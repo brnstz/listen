773eeda02b1d7d7fb5ff3e8c41ebb90acd53794c
Brian Seitz <brnstz@gmail.com>
2014-12-04 16:53:48 -0500

    moving code around to prep for web ui

--- backend.go
+++ backend.go
@@ -27,6 +27,18 @@ func main() {
 	go pullFromOMR()
 	go agg()
 
+	//http.HandleFunc("/api/shows.json", getShows)
+
+	// By default look for a static asset
+	//http.Handle("/", http.FileServer(http.Dir(staticDir)))
+
+	/*
+		err := http.ListenAndServe(":8003", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	*/
+
 	forever := make(chan bool)
 	<-forever
 }
--- web.go
+++ web.go
@@ -1,17 +1,21 @@
 package main
 
-import (
-	"encoding/json"
-	"log"
-	"net/http"
-
-	"github.com/brnstz/ohmy"
-)
-
 const (
 	// FIXME
 	staticDir = "/Users/bseitz/go/src/github.com/brnstz/sandbox/listen/html"
 )
+
+/*
+func getListings(listReq chan bool, resp chan []byte) {
+
+}
+
+func getShows(w http.ResponseWriter, r *http.Request) {
+	// Get
+	bucket := getBucket()
+	lpath := path.Join(rootPath, listingPath)
+
+}
 
 func getShows(w http.ResponseWriter, r *http.Request) {
 	shows, err := ohmy.GetShows(ohmy.RegionNYC, numShows)
@@ -33,18 +37,4 @@ func getShows(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 	}
 }
-
-/*
-func main() {
-	// Test API stuff
-	http.HandleFunc("/api/shows.json", getShows)
-
-	// By default look for a static asset
-	http.Handle("/", http.FileServer(http.Dir(staticDir)))
-
-	// FIXME
-	err := http.ListenAndServe(":8003", nil)
-	if err != nil {
-		panic(err)
-	}
-}*/
+*/
